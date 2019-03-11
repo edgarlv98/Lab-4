4df1bdc35b448853294c34ad426d9da468aba049
edgarlv98 <43259781+edgarlv98@users.noreply.github.com>
2019-03-10 20:16:05 -0600

    Delete credentials.js

--- credentials.js
+++
@@ -1,4 +0,0 @@
-
-module.exports.MAPBOX_TOKEN = 'pk.eyJ1IjoiZWRnYXI5OCIsImEiOiJjanQwYmwzYXAwZWphNDlrM2l0YndsOWFrIn0.d4euc9nX6iQjfkzqW5rEGQ'
-
-module.exports.DARK_SKY_SECRET_KEY = 'f36066403a6541b36499bfb3cdb35292'
